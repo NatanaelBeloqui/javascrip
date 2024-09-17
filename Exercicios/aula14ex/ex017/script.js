b4d5f45faa31a11a5e5715336692da20b3479746
Natanael Beloqui de Barros <nbdb2005@gmail.com>
2024-09-17 17:00:06 -0300

    Em progresso

---
+++ Exercicios/aula14ex/ex017/script.js
@@ -0,0 +1,13 @@
+function tabuada () {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+    if(num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+        }
+    }
+}
